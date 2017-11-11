844be059a418c0032ef5cfb5bbdd0316d70d91b8
whoami <whoami@hellsystem>
2017-11-11 11:49:49 +0100

    Add more test choice

--- test.sh
+++ test.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-NB_TEST=$1
-SIZE=$2
-NB_THREADS=$3
+function seq_tests {
+    COUNTER=0
+    NB_TEST=$1
+    SIZE=$2
+    echo "Test sequentiel sur $NB_TEST, taille de $SIZE";
+    while [  $COUNTER -lt $NB_TEST ]; do
+        ./creer_vecteur --size $SIZE | ./tri_sequentiel -t --quiet;
 
-COUNTER=0
-if [ $# -eq 0 ]; then
-    echo "Usage : script <nombre tests> <taille tableau> <nombre threads>";
+        let COUNTER=COUNTER+1
+    done
+}
+
+function thd_tests {
+    COUNTER=0
+    NB_TEST=$1
+    SIZE=$2
+    NB_THREADS=$3
+    echo "Test paralélisme ($NB_THREADS threads) sur $NB_TEST, taille de $SIZE";
+    while [  $COUNTER -lt $NB_TEST ]; do
+        ./creer_vecteur --size $SIZE |
+            ./tri_threads -t --quiet --parallelism $NB_THREADS;
+
+        let COUNTER=COUNTER+1
+    done
+}
+
+if [ $# -le 1 ]; then
+    echo "Usage : script <nombre tests> <taille tableau> [nombre threads]";
     exit;
+elif [ $# -eq 2 ]; then
+    seq_tests $1 $2;
+elif  [ $# -eq 3 ]; then
+    thd_tests $1 $2 $3;
 fi
-
-while [  $COUNTER -lt $NB_TEST ]; do
-    ./creer_vecteur --size $SIZE | ./tri_threads -t --quiet --parallelism $NB_THREADS;
-
-    let COUNTER=COUNTER+1 
-done
