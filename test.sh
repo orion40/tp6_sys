8f8ae1f56c8ec0abe41bbe9fd7ed6f7376ac0405
whoami <whoami@hellsystem>
2017-11-16 06:53:00 +0100

    Simplify code and switch to size;time;run_id output format

--- test.sh
+++ test.sh
@@ -1,36 +1,49 @@
 #!/bin/bash
 
-function seq_tests {
-    COUNTER=0
-    NB_TEST=$1
-    SIZE=$2
-    echo "Test sequentiel sur $NB_TEST, taille de $SIZE";
-    while [  $COUNTER -lt $NB_TEST ]; do
-        ./creer_vecteur --size $SIZE | ./tri_sequentiel -t --quiet;
+BEGIN_SIZE=1000
 
-        let COUNTER=COUNTER+1
-    done
+function seq {
+    ./creer_vecteur --size $1 | ./tri_sequentiel -t --quiet;
 }
 
-function thd_tests {
+function thd {
+    ./creer_vecteur --size $1 |
+        ./tri_threads -t --quiet --parallelism $2;
+}
+
+function main {
     COUNTER=0
     NB_TEST=$1
-    SIZE=$2
-    NB_THREADS=$3
-    echo "Test paralélisme ($NB_THREADS threads) sur $NB_TEST, taille de $SIZE";
-    while [  $COUNTER -lt $NB_TEST ]; do
-        ./creer_vecteur --size $SIZE |
-            ./tri_threads -t --quiet --parallelism $NB_THREADS;
-
-        let COUNTER=COUNTER+1
+    SIZE=$BEGIN_SIZE
+    MAX_SIZE=$2
+    NB_THREADS="$3"
+    echo "size;temps;run_id"
+    while [ $SIZE -le $MAX_SIZE ]; do
+        while [  $COUNTER -lt $NB_TEST ]; do
+            if [ -z $NB_THREADS ]; then
+                seq $SIZE;
+            else
+                thd $SIZE $NB_THREADS;
+            fi
+            let COUNTER=COUNTER+1
+            echo ";$COUNTER"
+        done
+        let SIZE=SIZE*2
+        let COUNTER=0
     done
+
 }
 
 if [ $# -le 1 ]; then
-    echo "Usage : script <nombre tests> <taille tableau> [nombre threads]";
+    echo "Usage : script <nombre tests> <taille tableau max> [nombre threads]";
     exit;
-elif [ $# -eq 2 ]; then
-    seq_tests $1 $2;
+fi
+if [ $2 -lt $BEGIN_SIZE ]; then
+    echo "Value is too small.";
+    exit;
+fi
+if [ $# -eq 2 ]; then
+    main $1 $2;
 elif  [ $# -eq 3 ]; then
-    thd_tests $1 $2 $3;
+    main $1 $2 $3;
 fi
