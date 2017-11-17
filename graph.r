3c0d5713c39232c25ef58109fdd96b58792e47bc
whoami <whoami@hellsystem>
2017-11-17 13:21:55 +0100

    Add other csvs

--- graph.r
+++ graph.r
@@ -2,81 +2,55 @@ library(ggplot2)
 library(plyr)
 library(reshape2)
 
+# obtention des données
 data_size<- data.frame(read.csv2(file="varying_size.csv",sep=';', dec='.'));
 data_thd<- data.frame(read.csv2(file="varying_thd.csv",sep=';', dec='.'));
+rusage_size<- data.frame(read.csv2(file="vsize_rusage.csv",sep=';', dec='.'));
+rusage_thd<- data.frame(read.csv2(file="vthd_rusage.csv",sep=';', dec='.'));
 
 stat_size<-ddply(data_size,c("size"),summarise,N=length(temps),mean=mean(temps))
 stat_thd<-ddply(data_thd,c("num_thread"),summarise,N=length(temps),mean=mean(temps))
-p <- ggplot();
-q <- ggplot();
+stat_rusage_size<-ddply(rusage_size,c("num_thread"),summarise,N=length(temps),mean=mean(temps))
+stat_rusage_thd<-ddply(rusage_thd,c("num_thread"),summarise,N=length(temps),mean=mean(temps))
 
-p <- p + geom_point(data=stat_size, group=1, aes(x=size, y=mean, colour = "Taille des tableaux")) + geom_line(data=stat_size, aes(x=size, y=mean, colour = "Taille des tableaux"));
-q <- q + geom_point(data=stat_thd, group=1, aes(x=num_thread, y=mean, colour = "Nombre de thread")) + geom_line(data=stat_thd, aes(x=num_thread, y=mean, colour = "Nombre de thread"));
+gettime_var_table <- ggplot();
+gettime_var_thd <- ggplot();
+rusage_var_table <- ggplot();
+rusage_var_thd <- ggplot();
 
-p <- p + xlab("Taille des tableaux")
-p <- p + ylab("Temps de calcul (µs)")
+gettime_var_table <- gettime_var_table + geom_point(data=stat_size, group=1, aes(x=size, y=mean, colour = "Taille des tableaux")) + geom_line(data=stat_size, aes(x=size, y=mean, colour = "Taille des tableaux"));
+gettime_var_thd <- gettime_var_thd + geom_point(data=stat_thd, group=1, aes(x=num_thread, y=mean, colour = "Nombre de thread")) + geom_line(data=stat_thd, aes(x=num_thread, y=mean, colour = "Nombre de thread"));
 
-p <-  p + theme(panel.background = element_rect(fill = "lightgrey"),
+gettime_var_table <- gettime_var_table + xlab("Taille des tableaux")
+gettime_var_table <- gettime_var_table + ylab("Temps de calcul (µs)")
+
+gettime_var_table <-  gettime_var_table + theme(panel.background = element_rect(fill = "lightgrey"),
                 panel.grid.minor = element_line(colour = "black",
                 linetype="dashed", size = 0.1),
                 panel.grid.major = element_line(colour = "black", size = 0.1),
                 legend.position="bottom")
 
 cols_table <- c("Taille des tableaux"="red")
-p <- p + scale_colour_manual("",
+gettime_var_table <- gettime_var_table + scale_colour_manual("",
                              values = cols_table)
-p <- p + ggtitle("Variation du temps de calcul sur 4 threads")
+gettime_var_table <- gettime_var_table + ggtitle("Variation du temps de calcul sur 4 threads")
 
-q <- q + xlab("Nombre de thread")
-q <- q + ylab("Temps de calcul (µs)")
+gettime_var_thd <- gettime_var_thd + xlab("Nombre de thread")
+gettime_var_thd <- gettime_var_thd + ylab("Temps de calcul (µs)")
 
-q <-  q + theme(panel.background = element_rect(fill = "lightgrey"),
+gettime_var_thd <-  gettime_var_thd + theme(panel.background = element_rect(fill = "lightgrey"),
                 panel.grid.minor = element_line(colour = "black",
                 linetype="dashed", size = 0.1),
                 panel.grid.major = element_line(colour = "black", size = 0.1),
                 legend.position="bottom")
 
 cols_threads <- c("Nombre de thread"="red")
-q <- q + scale_colour_manual("",
+gettime_var_thd <- gettime_var_thd + scale_colour_manual("",
                              values = cols_threads)
 
-q <- q + ggtitle("Variation du temps de calcul sur 100000 éléments")
+gettime_var_thd <- gettime_var_thd + ggtitle("Variation du temps de calcul sur 100000 éléments")
 
-# p <- p + scale_colour_manual("", 
-#                              values = cols)
-# 
-# p <-  p + theme(panel.background = element_rect(fill = "lightgrey"),
-#                 panel.grid.minor = element_line(colour = "black", linetype="dashed", size = 0.1),
-#                 panel.grid.major = element_line(colour = "black", size = 0.1),
-#                 legend.position="bottom")
-
-p
-q
-# stat_naif<-ddply(data_para_naif,c("Taille_mat"),summarise,N=length(Time),mean=mean(Time),sd=sd(Time),se=1.96*(sd/sqrt(N)))
-
-# script R permettant de comparer les temps moyens de calcul des produits
-# de matrices carrées entre algo classique et algo transposée
-# pour des tailles variant de 2000 à 5000
-
-# analyse statistique
-
-# stat_naif<-ddply(data_para_naif,c("Taille_mat"),summarise,N=length(Time),mean=mean(Time),sd=sd(Time),se=1.96*(sd/sqrt(N)))
-
-# tracé des résultats obtenus
-# p <- ggplot()
-# p <- p + geom_point(data=stat_naif, aes(x=Taille_mat, y=mean, colour = "algorithme naïf")) + geom_line(data=stat_naif, aes(x=Taille_mat, y=mean, colour = "algorithme naïf")) 
-# p <- p + geom_errorbar(data=stat_naif,aes(x=Taille_mat,ymin=mean-se,ymax=mean+se),width=.2,position=position_dodge())
-# 
-# 
-# p <- p + xlab("Taille des matrices")
-# p <- p + ylab("Temps de calcul (s)")
-# 
-# cols <- c("algorithme naïf"="red","algorithme transposée"="blue")
-# p <- p + scale_colour_manual("", 
-#                              values = cols)
-# 
-# p <-  p + theme(panel.background = element_rect(fill = "lightgrey"),
-#                 panel.grid.minor = element_line(colour = "black", linetype="dashed", size = 0.1),
-#                 panel.grid.major = element_line(colour = "black", size = 0.1),
-#                 legend.position="bottom")
-#show(p)
+gettime_var_table
+gettime_var_thd
+rusage_var_table
+rusage_var_thd
